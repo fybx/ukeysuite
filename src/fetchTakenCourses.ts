2df7f43e963d9db9953ec18c3134a53f56d6a2b6
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-10-09 17:32:23 +0300

    wrapper for UkeyInstance fetchCourses
    Signed-off-by: Ferit Yiğit BALABAN <fybalaban@fybx.dev>

--- src/fetchTakenCourses.ts
+++ src/fetchTakenCourses.ts
@@ -5,6 +5,20 @@ const selectorUlDersler =
     'body > div > aside > section > div > div > div:nth-child(1) > div > div.inner > ul';
 const urlMainPage = 'https://ukey.uludag.edu.tr/KontrolPaneli/';
 
+/** Gathers taken courses from the unordered list in mainpage.
+ * @param {string} credentialsPath Path to credentials file
+ * @returns {Promise<CourseWithoutItems[] | undefined>} An array of course objects
+ */
+export async function fetchCourses(
+    credentialsPath: string
+): Promise<CourseWithoutItems[] | undefined> {
+    return loginToUkey(getCredentials(credentialsPath))
+        .then((result) => fetchCoursesInstance(result))
+        .catch((err) => {
+            console.error('An error occured while logging in:', err);
+            return undefined;
+        });
+}
 
 /** Gathers taken courses from the unordered list in mainpage.
  * @param {UkeyInstance} loggedInInstance An UkeyInstance
