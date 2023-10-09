f814eef295264a204cbfbfcd65e862fbf82851bb
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-10-09 17:31:56 +0300

    course fetching logic with UkeyInstance
    Signed-off-by: Ferit Yiğit BALABAN <fybalaban@fybx.dev>

--- src/fetchTakenCourses.ts
+++ src/fetchTakenCourses.ts
@@ -5,3 +5,38 @@ const selectorUlDersler =
     'body > div > aside > section > div > div > div:nth-child(1) > div > div.inner > ul';
 const urlMainPage = 'https://ukey.uludag.edu.tr/KontrolPaneli/';
 
+
+/** Gathers taken courses from the unordered list in mainpage.
+ * @param {UkeyInstance} loggedInInstance An UkeyInstance
+ * @returns {Promise<CourseWithoutItems[] | undefined>} An array of course objects
+ */
+export async function fetchCoursesInstance(
+    loggedInInstance: UkeyInstance
+): Promise<CourseWithoutItems[] | undefined> {
+    if (!loggedInInstance) {
+        console.error('TODO!');
+        return undefined;
+    }
+
+    await Promise.all([
+        loggedInInstance.page.goto(urlMainPage),
+        loggedInInstance.page.waitForNavigation(),
+    ]);
+
+    return loggedInInstance.page.evaluate((selectorUl) => {
+        const ul = document.querySelector(selectorUl);
+        const aElements = ul!.querySelectorAll('a');
+        return Array.from(aElements)
+            .map((a) => {
+                if (a) {
+                    const [courseCode, courseName] = a.innerText.split(' - ', 2);
+                    return {
+                        href: a.href,
+                        courseName,
+                        courseCode,
+                    };
+                }
+            })
+            .filter((el) => el !== undefined) as CourseWithoutItems[];
+    }, selectorUlDersler);
+}
