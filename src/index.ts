50a8db8caf61b767e54324fc9f106b5133ded59f
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-10-09 21:12:39 +0300

    added exports for package
    Signed-off-by: Ferit Yiğit BALABAN <fybalaban@fybx.dev>

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export {getItems, getItemsInstance, createCourseDatabaseFile} from './courseActions';
+export {fetchCourses, fetchCoursesInstance} from './fetchTakenCourses';
+export {writeCredentials, getCredentials, loginToUkey} from './requiredUtility';
+export {UkeyInstance, CourseItem, CourseItemDatabase, CourseWithItems, CourseWithoutItems, Credentials} from './types';
