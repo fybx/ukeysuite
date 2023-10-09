1a7eac8d7fc45fa96738b448d802f8bd07c331ef
Ferit Yiğit BALABAN <fybalaban@fybx.dev>
2023-10-09 17:30:27 +0300

    credential operations
    Signed-off-by: Ferit Yiğit BALABAN <fybalaban@fybx.dev>

--- src/requiredUtility.ts
+++ src/requiredUtility.ts
@@ -7,3 +7,16 @@ const selectorRadioOgrenci =
     'body > div > div.row > div > div.body.col-lg-12 > div.row.bg-white.no-padding > div.pull-right.col-lg-5 > form > div.form-group.text-black.text-justify > input:nth-child(1)';
 const selectorButtonLogin =
     'body > div > div.row > div > div.body.col-lg-12 > div.row.bg-white.no-padding > div.pull-right.col-lg-5 > form > div:nth-child(4) > button';
+
+/** Opens and reads the credentials file, returning it as a dictionary.
+ * @param {string} credentialsPath Path to credentials file
+ * @returns {Credentials} An object of username and password
+ */
+export function getCredentials(credentialsPath: string): Credentials {
+    return JSON.parse(readFileSync(credentialsPath, 'utf-8'));
+}
+
+export function writeCredentials(credentialsPath: string, credentials: Credentials): void {
+    writeFileSync(credentialsPath, JSON.stringify(credentials, null, 4));
+}
+
